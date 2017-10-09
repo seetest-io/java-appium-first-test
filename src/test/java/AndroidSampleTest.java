57e7f83ea25d4fe7dbea9a3520e0f633828e0d29
nivi.mor <nivi.mor@experitest.com>
2017-10-09 09:51:10 +0300

    added functionality to tests

--- src/test/java/AndroidSampleTest.java
+++ src/test/java/AndroidSampleTest.java
@@ -4,6 +4,7 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -21,19 +22,31 @@ public class AndroidSampleTest {
         dc.setCapability("testName", testName);
         dc.setCapability("accessKey", accessKey);
         //install the app on the device
-        dc.setCapability(MobileCapabilityType.APP, "cloud:<BUNDLE_ID>");
+        dc.setCapability(MobileCapabilityType.APP, "cloud:com.experitest.ExperiBank/.LoginActivity");
         //get an Android device
         dc.setCapability("platformName", "Android");
         //launch the app
-        dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "<BUNDLE_ID>");
-        dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "<ACTIVITY>");
+        dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.experitest.ExperiBank");
+        dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".LoginActivity");
         driver = new AndroidDriver(new URL("https://cloud.experitest.com:443/wd/hub"), dc);
     }
 
     @Test
     public void testYourAndroidApp() {
-
-        //Write your test case here
+        if(driver.isLocked()){
+            driver.unlockDevice();
+        }
+        driver.findElement(By.xpath("//*[@id='usernameTextField']")).sendKeys("company");
+        driver.findElement(By.xpath("//*[@id='passwordTextField']")).sendKeys("company");
+        driver.findElement(By.xpath("//*[@id='loginButton']")).click();
+        driver.findElement(By.xpath("//*[@id='makePaymentButton']")).click();
+        driver.findElement(By.xpath("//*[@id='phoneTextField']")).sendKeys("123456");
+        driver.findElement(By.xpath("//*[@id='nameTextField']")).sendKeys("Test");
+        driver.findElement(By.xpath("//*[@id='amountTextField']")).sendKeys("10");
+        driver.findElement(By.xpath("//*[@id='countryTextField']")).sendKeys("US");
+        driver.hideKeyboard();
+        driver.findElement(By.xpath("//*[@id='sendPaymentButton']")).click();
+        driver.findElement(By.xpath("//*[@text='Yes']")).click();
     }
 
     @After
--- src/test/java/iOSSampleTest.java
+++ src/test/java/iOSSampleTest.java
@@ -5,6 +5,7 @@ import org.boon.core.Sys;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -13,7 +14,7 @@ public class iOSSampleTest{
 
     IOSDriver driver = null;
     DesiredCapabilities dc = new DesiredCapabilities();
-    String testName = "<TEST_NAME>";
+    String testName = "Testing iOS App";
     String accessKey = System.getenv("accessKey");
 
     @Before
@@ -22,18 +23,27 @@ public class iOSSampleTest{
         dc.setCapability("testName", testName);
         dc.setCapability("accessKey", accessKey);
         //install the app on the device
-        dc.setCapability(MobileCapabilityType.APP, "cloud:<BUNDLE_ID>");
+        dc.setCapability(MobileCapabilityType.APP, "cloud:com.experitest.ExperiBank");
         //get an iOS device
         dc.setCapability("platformName", "iOS");
         //launch the app
-        dc.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "<BUNDLE_ID>");
+        dc.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.experitest.ExperiBank");
         driver = new IOSDriver(new URL("https://cloud.experitest.com:443/wd/hub"), dc);
     }
 
     @Test
     public void testYouriOSApp() {
 
-        //Write your test case here
+        driver.findElement(By.xpath("//*[@text='Username']")).sendKeys("company");
+        driver.findElement(By.xpath("//*[@text='Password']")).sendKeys("company");
+        driver.findElement(By.xpath("//*[@text='loginButton']")).click();
+        driver.findElement(By.xpath("//*[@text='makePaymentButton']")).click();
+        driver.findElement(By.xpath("//*[@text='Phone']")).sendKeys("123456");
+        driver.findElement(By.xpath("//*[@text='Name']")).sendKeys("Test");
+        driver.findElement(By.xpath("//*[@text='Amount']")).sendKeys("10");
+        driver.findElement(By.xpath("//*[@text='Country']")).sendKeys("US");
+        driver.findElement(By.xpath("//*[@text='sendPaymentButton']")).click();
+        driver.findElement(By.xpath("//*[@text='Yes']")).click();
     }
 
     @After
